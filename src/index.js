24f3272469537202ad4bd423c8e57043cfa01a04
greyGroot <grey.rsm@gmail.com>
2022-01-31 21:34:51 +0200

    4 setup webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // https://youtu.be/Ttf3CEsEwMQ
 // Selectors
 
+import '../styles/index.css'
+import '../index.html'
+
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
